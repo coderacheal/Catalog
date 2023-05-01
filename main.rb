c64c744e244f139426d7f0dba1ebfcadf9e526fb
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-05-01 16:57:28 +0000

    Add main and app files

--- main.rb
+++ main.rb
@@ -1,9 +1,21 @@
 require_relative './modules/app'
 
-
 def main
   app = App.new
-  app.all_otions
+  app.program_title
+  option = ''
+
+  exit_program = lambda {
+    puts 'BYE!!! SEE YOU WHEN YOU HAVE NEW THINGS TO ARCHIVE!'
+    exit
+  }
+
+  while option != '0'
+    app.all_options
+    option = gets.chomp
+    app.execute_option unless option == '0'
+  end
+  exit_program.call
 end
 
 main
--- modules/app.rb
+++ modules/app.rb
@@ -1,19 +1,24 @@
-class App 
+class App
+  def program_title
+    puts '======== CATALOG OF THINGS I OWN =========='
+  end
 
-    def all_otions
-        puts '========CATALOG OF THINGS I OWN=========='
-        puts ''
-        puts '1 - List all books'
-        puts '2 - List all music albums'
-        puts '3 - List all movies'
-        puts '4 - List of games'
-        puts '5 - List all genres (e.g Comedy, Thriller)'
-        puts '6 - List all labels (e.g. Gift, New)'
-        puts '7 - List all authors (e.g. Stephen King)'
-        puts '8 - List all sources (e.g. From a friend, Online shop)'
-        puts '9 - Add a book'
-        puts '10 - Add a music album'
-        puts '11 - Add a movie'
-        puts '12 - Add a game'
-    end
+  def all_options
+    puts ''
+    puts '1 - List all books'
+    puts '2 - List all music albums'
+    puts '3 - List all movies'
+    puts '4 - List of games'
+    puts '5 - List all genres (e.g Comedy, Thriller)'
+    puts '6 - List all labels (e.g. Gift, New)'
+    puts '7 - List all authors (e.g. Stephen King)'
+    puts '8 - List all sources (e.g. From a friend, Online shop)'
+    puts '9 - Add a book'
+    puts '10 - Add a music album'
+    puts '11 - Add a movie'
+    puts '12 - Add a game'
+    puts '0 - Enter 0 to quit'
+  end
+
+  def execute_option(option); end
 end
